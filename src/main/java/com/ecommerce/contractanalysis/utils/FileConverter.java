bb8b2d6a2dcece82dd36c0ff5fc06497a404b5d8
Petr <petr.chromecka.s01@osu.cz>
2025-09-18 13:27:18 +0200

    added FileConverter utility for extracting text from uploaded files

---
+++ src/main/java/com/ecommerce/contractanalysis/utils/FileConverter.java
@@ -0,0 +1,12 @@
+package com.ecommerce.contractanalysis.utils;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class FileConverter {
+
+    public String extractText(MultipartFile file) {
+        return null; //todo add extraction from the pdf or docx
+    }
+}
