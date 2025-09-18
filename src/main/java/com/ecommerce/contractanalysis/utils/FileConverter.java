e061fd700ac6987badceeaa4a91b538be9d80059
Petr <petr.chromecka.s01@osu.cz>
2025-09-18 17:18:02 +0200

    added logic to extractText method with Tika

--- src/main/java/com/ecommerce/contractanalysis/utils/FileConverter.java
+++ src/main/java/com/ecommerce/contractanalysis/utils/FileConverter.java
@@ -1,12 +1,29 @@
 package com.ecommerce.contractanalysis.utils;
 
+import org.apache.commons.io.FilenameUtils;
+import org.apache.tika.Tika;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Component
 public class FileConverter {
+    private final Tika tika = new Tika();
 
     public String extractText(MultipartFile file) {
-        return null; //todo add extraction from the pdf or docx
+        if (file == null || file.isEmpty()) {
+            throw new IllegalArgumentException("Soubor je prázdný nebo chybí");
+        }
+
+        final String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+        SupportedFileType.fromExtension(extension);
+
+        try {
+            return tika.parseToString(file.getInputStream());
+        } catch (Exception ex) {
+            throw new IllegalArgumentException(
+                    "Chyba při čtení souboru: " + file.getOriginalFilename(), ex
+            );
+        }
     }
+
 }
